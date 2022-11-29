d73f4a69ce4635d2919e6e550cf3998a77f8eac4
Craig Donald <1craig.donald@gmail.com>
2022-11-29 14:48:46 +0000

    Working GET & POST for /artists

---
+++ routes.md
@@ -0,0 +1,43 @@
+## List all albums/artists
+
+Request:
+GET /albums
+
+GET /artists
+
+Response (200 OK):
+a list of albums/artists
+
+
+## Find a specific album/artist
+
+Request:
+
+GET /albums/:id
+GET /artists/:id
+
+Response (200 OK):
+One specific album/artist with the right ID
+
+## Create a new album
+
+Request
+POST /albums
+
+With body parameters:
+    title="Songs for the Deaf"
+    release_year="2004"
+    artist_id="3" 
+
+Response (200 OK):
+No content, just creates new resource (new album)
+
+<!-- BEGIN GENERATED SECTION DO NOT EDIT -->
+
+---
+
+**How was this resource?**  
+[😫](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fweb-applications&prefill_File=resources%2Fmusic_library_database_app%2Froutes.md&prefill_Sentiment=😫) [😕](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fweb-applications&prefill_File=resources%2Fmusic_library_database_app%2Froutes.md&prefill_Sentiment=😕) [😐](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fweb-applications&prefill_File=resources%2Fmusic_library_database_app%2Froutes.md&prefill_Sentiment=😐) [🙂](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fweb-applications&prefill_File=resources%2Fmusic_library_database_app%2Froutes.md&prefill_Sentiment=🙂) [😀](https://airtable.com/shrUJ3t7KLMqVRFKR?prefill_Repository=makersacademy%2Fweb-applications&prefill_File=resources%2Fmusic_library_database_app%2Froutes.md&prefill_Sentiment=😀)  
+Click an emoji to tell us.
+
+<!-- END GENERATED SECTION DO NOT EDIT -->
---
+++ spec/integration/application_spec.rb
@@ -0,0 +1,53 @@
+require "spec_helper"
+require "rack/test"
+require_relative '../../app'
+
+describe Application do
+  # This is so we can use rack-test helper methods.
+  include Rack::Test::Methods
+
+  # We need to declare the `app` value by instantiating the Application
+  # class so our tests work.
+  let(:app) { Application.new }
+
+  context 'GET /albums' do
+    it 'should return a list of albums' do
+      response = get('/albums')
+      expected_response = 'Surfer Rosa, Waterloo, Super Trouper, Bossanova, Lover, Folklore, I Put a Spell on You, Baltimore, Here Comes the Sun, Fodder on My Wings, Ring Ring'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_response)
+    end
+  end
+
+  context 'Post /albums' do
+    it 'should add a new album to the list' do
+      response = post('/albums',title: 'OK Computer', release_date: '1997', artist_id: '1')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('New album added')
+
+      check = get('/albums')
+      expect(check.body).to include('OK Computer')
+    end
+  end
+
+  context "GET /artists" do
+    it 'returns 200 OK and list of artists' do
+      response = get('/artists')
+      expected_return = 'Pixies, ABBA, Taylor Swift, Nina Simone, Kiasmos'
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq(expected_return)
+    end
+  end
+
+  context 'POST /artists/' do
+    it 'adds a new artist' do
+      response = post('/artists')
+
+      expect(response.status).to eq(200)
+      expect(response.body).to eq('New artist added')
+    end
+  end
+end
